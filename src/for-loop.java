15f3442f3d65c6977bc7d1fa426c21b5239b878e
Zodiac <zodiac_gu@163.com>
2017-05-17 23:49:07 +0800

    Add for loop demo by java

---
+++ src/for-loop.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		int i,j;
+		for (i=0;i<100;i++)
+			for(j=0;j<10;j++)
+				if(i==j*j){
+					System.out.printf("i=%d,j=%d\n",i,j);
+				}
+	}
+
+}
