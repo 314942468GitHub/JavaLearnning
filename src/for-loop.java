0c5a8b3f8d2961221eae4dd405de59b106797404
Zodiac <zodiac_gu@163.com>
2017-05-18 21:49:21 +0800

    update for loop

--- src/for-loop.java
+++ src/for-loop.java
@@ -1,10 +1,17 @@
-public class HelloWorld{
-	public static void main(String[] args){
+public class HelloWorld {
+	public static void main(String[] args) {
 		int i,j;
 		for (i=0;i<100;i++)
 			for(j=0;j<10;j++)
-				if(i==j*j){
-					System.out.printf("i=%d,j=%d\n",i,j);
-				}
+					System.out.printf("%d-%d=%d,%d+%d=%d\n",i,j,minus(i,j),i,j,add(i,j));
+
+	}
+	
+	static public int minus(int x,int y){
+		return (x-y);
+	}
+	
+	static public int add(int x,int y){
+		return (x+y);
 	}
 }
